804c7c7dcafad79eb89b6e5387d2cad4fd80828b
Ghassen Saaf <saafghassen@gmail.com>
2023-06-21 19:06:11 +0100

    graphql module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { KafkaModule } from './kafka/kafka.module';
+import { DateTimeResolver } from 'graphql-scalars';
 
 @Module({
-  imports: [KafkaModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      typePaths: ['./**/*.graphql'],
+      resolvers: {
+        DateTime: DateTimeResolver,
+      },
+    }),
+    KafkaModule,
+  ],
   controllers: [],
   providers: [],
 })
