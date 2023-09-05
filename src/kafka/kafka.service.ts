ac0054d1d9a4fa65a41664ad6919921f4665b7f8
Ghassen Saaf <saafghassen@gmail.com>
2023-09-05 22:34:56 +0100

    changes

--- src/kafka/kafka.service.ts
+++ src/kafka/kafka.service.ts
@@ -36,6 +36,7 @@ export class KafkaService {
     await consumer.run({
       eachMessage: async ({ topic, partition, message, heartbeat }) => {
         const payload = JSON.parse(message.value.toString());
+        log(payload);
         this.notificationService.create({
           title: payload.payload.title,
           body: payload.payload.body,
